94ec629ff0617bd75e688a89b936a31c03366434
manish mandal <76776194mm@gmail.com>
2023-04-07 23:14:13 +0530

    textarea markup added

--- src/js/views/addReplyView.js
+++ src/js/views/addReplyView.js
@@ -1,13 +1,44 @@
 class addReplyView {
+	_data;
+	_parentElement;
 	_body = document.querySelector('body');
+
+	render(data) {
+		this._data = data;
+		console.log(this._data);
+		const { currentUser, container } = this._data;
+		this._parentElement = container;
+		console.log(this._parentElement);
+		this.textareaMarkUp(currentUser);
+	}
 
 	addHandlerReplyClick(handler) {
 		this._body.addEventListener('click', (e) => {
 			const replyBtn = e.target.closest('.replyBtn');
-			console.log(replyBtn);
+			const replyContainer = e.target.closest('.main__replies-list');
+			const commentContainer = e.target.closest('.comments-container__section');
 			if (!replyBtn) return;
-			handler();
+			if (commentContainer) {
+				handler(commentContainer);
+				return;
+			}
+			replyContainer && handler(replyContainer);
 		});
+	}
+
+	textareaMarkUp(currentUser) {
+		const markup = `
+		<section class="comments-container__write-comment">
+			<form class="form">
+				<label for="write-comment">
+				<img class="writer-dp" src="${currentUser.image.png}" alt="writer-dp">
+				</label>
+				<textarea type="text" name="comment" class="textarea-filed" id="write-comment" placeholder="Add a comment..."></textarea>
+				<button class="comment-btn" type="submit">send</button>
+			</form>
+    	</section>
+		`;
+		this._parentElement.insertAdjacentHTML('afterEnd', markup);
 	}
 }
 
