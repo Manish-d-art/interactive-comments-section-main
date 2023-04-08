d4d5fe6fd732aab878d93718c2c515647f66e20d
manish mandal <76776194mm@gmail.com>
2023-04-08 17:27:13 +0530

    modified

--- src/js/views/addCommentView.js
+++ src/js/views/addCommentView.js
@@ -1,65 +1,16 @@
-import deleteIcon from '/src/assets/images/icon-delete.svg';
-import editIcon from '/src/assets/images/icon-edit.svg';
+import View from './view.js';
 
-class addCommentView {
-	_parentElement = document.querySelector('.comments-container');
-	_form = document.querySelector('.form');
-	_textarea = document.querySelector('.textarea-filed');
-	_data;
-
-	render(data) {
-		this._data = data;
-		const { comment, currentUser } = this._data;
-		this._textareaMarkUp(currentUser, comment);
-		this._textarea.value = '';
-	}
+class addCommentView extends View {
+	parentElement = document.querySelector('.comments-container');
+	textarea = document.querySelector('.textarea-fieled');
+	form = document.querySelector('.form');
+	data;
 
 	addHandlerComment(handler) {
-		this._textarea.focus();
-		this._form.addEventListener('submit', function (e) {
+		this.form.addEventListener('submit', (e) => {
 			e.preventDefault();
 			handler();
 		});
-	}
-
-	getComment() {
-		const text = this._textarea.value;
-		return text;
-	}
-
-	_textareaMarkUp(currentUser, comment) {
-		const markup = `
-        <li>
-        <section class="main__replies">
-          <div class="comments-container__user">
-            <img class="comments-container__user-dp" src="${currentUser.image.png}" alt="user-dp">
-            <p class="comments-container__user-name">juliusomo</p>
-            <p class="comments-container__user-time">2 days ago</p>
-         </div>
-         <p class="comments-container__section-content">
-            ${comment}
-         </p>
-         <div class="comments-container__section__vote">
-            <button class="up-btn">+</button>
-            <p class="score">12</p>
-            <button class="down-btn">-</button>
-         </div>
-        <div class="comments-container__section__replyNmodify">
-          <button class="delete-btn">
-            <img class="delete-icon" src="${deleteIcon}" alt="delete-icon">
-            delete
-          </button>
-          <button class="edit-btn"> 
-            <img class="edit-icon" src="${editIcon}" alt="edit-icon">
-            edit
-          </button>
-        </div>
-        </section>
-      </li>
-      <ul class="main__replies-container">
-      </ul>
-        `;
-		this._parentElement.insertAdjacentHTML('beforeend', markup);
 	}
 }
 
