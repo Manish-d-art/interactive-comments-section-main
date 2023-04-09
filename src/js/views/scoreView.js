452f7cfdc4e6d1701c791c922e6e1be6d2dece76
manish mandal <76776194mm@gmail.com>
2023-04-09 23:37:13 +0530

    add scoreView.js

---
+++ src/js/views/scoreView.js
@@ -0,0 +1,37 @@
+import View from './view.js';
+
+class ScoreView extends View {
+	_score;
+	_parentEle;
+	_plusBtn;
+	_minusBtn;
+
+	addHandlerClick(handler) {
+		this.body.addEventListener('click', (e) => {
+			this._plusBtn = e.target.closest('.plus-btn');
+			this._minusBtn = e.target.closest('.minus-btn');
+			if (!this._plusBtn && !this._minusBtn) return;
+			this._setScoreProperty(e);
+			handler();
+		});
+	}
+
+	_findParentElement(e) {
+		return e.target.closest('.comments-container__section__vote');
+	}
+
+	_setScoreProperty(e) {
+		const parentEle = this._findParentElement(e);
+		if (!parentEle) return;
+		this._score = parentEle.querySelector('.score');
+	}
+
+	updateScore() {
+		let scoreValue = +this._score.textContent;
+		if (this._plusBtn) scoreValue += 1;
+		if (this._minusBtn) scoreValue -= 1;
+		this._score.textContent = scoreValue;
+	}
+}
+
+export default new ScoreView();
