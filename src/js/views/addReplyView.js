8ea163ad3d17dac8d54f72b03e4da09db5eff32e
manish mandal <76776194mm@gmail.com>
2023-04-11 15:16:53 +0530

    update

--- src/js/views/addReplyView.js
+++ src/js/views/addReplyView.js
@@ -1,21 +1,25 @@
 import View from './view.js';
 
 class addReplyView extends View {
-	body = document.querySelector('.comments-container');
+	commentsContainer = document.querySelector('.comments-container');
 	parentElement;
 	textarea;
 
 	addHandlerReply(handler) {
-		this.body.addEventListener('click', (e) => {
+		this.commentsContainer.addEventListener('click', (e) => {
 			e.preventDefault();
 			const btn = e.target.closest('.comment-btn');
 			if (!btn) return;
 			this.textarea = e.target
 				.closest('.comments-container__write-comment')
 				.querySelector('.textarea-fieled');
-			this.parentElement = e.target
-				.closest('.comments-container__list')
-				.querySelector('.main__replies-container');
+			// this.parentElement = e.target
+			// .closest('.comments-container__list')
+			// .querySelector('.main__replies-container');
+			this.parentElement = e.target.closest(
+				'.comments-container__write-comment'
+			);
+			console.log(this.parentElement);
 			handler();
 		});
 	}
