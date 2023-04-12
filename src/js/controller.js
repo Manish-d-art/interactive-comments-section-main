92a605bc56aa3cb69ac9e5beca414f2da8209f66
manish mandal <76776194mm@gmail.com>
2023-04-12 09:06:54 +0530

    modified

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,9 @@ import commentsView from './views/commentsView.js';
 import addCommentView from './views/addCommentView.js';
 import replyTextareaView from './views/replyTextareaView.js';
 import addReplyView from './views/addReplyView.js';
+import deleteCommentView from './views/deleteCommentView.js';
+import scoreView from './views/scoreView.js';
+import editCommentView from './views/editCommentView.js';
 
 const controlInitialComments = () => {
 	const { comments } = model.state;
@@ -27,10 +30,27 @@ const controlAddReply = () => {
 	addReplyView.removeTextarea();
 };
 
+const controlDeleteComment = () => {
+	deleteCommentView.removeComment();
+};
+
+const controlScore = () => {
+	scoreView.updateScore();
+};
+
+const controlEditComment = () => {
+	console.log(123);
+	const { currentUser } = model.state;
+	editCommentView.render(currentUser);
+};
+
 const init = function () {
 	controlInitialComments();
 	addCommentView.addHandlerComment(controlAddComment);
 	replyTextareaView.addHandlerReplyClick(controlAddReplyMarkUp);
 	addReplyView.addHandlerReply(controlAddReply);
+	deleteCommentView.addHandlerDelete(controlDeleteComment);
+	scoreView.addHandlerClick(controlScore);
+	editCommentView.addHandlerEdit(controlEditComment);
 };
 init();
