76b9a830f101b61cf9a69a1372d795c4f13e80f9
manish mandal <76776194mm@gmail.com>
2023-04-22 10:54:48 +0530

    updated individual textarea field

--- src/js/views/replyTextareaView.js
+++ src/js/views/replyTextareaView.js
@@ -2,61 +2,66 @@ import View from './view.js';
 
 class addReplyTextareaView extends View {
 	_data;
-	_parentElement;
-	_body = document.querySelector('body');
+	parentElement;
 
 	render(data) {
 		this._data = data;
-		console.log(this._data);
+		// console.log(this._data);
 		const currentUser = this._data;
 		this.textareaMarkUp(currentUser);
 	}
 
-	removeActiveBtns() {
-		const btns = document.querySelectorAll('.replyBtn');
-		btns.forEach((btn) => btn.classList.remove('replyBtn--active'));
-	}
-
 	addHandlerReplyClick(handler) {
-		this._body.addEventListener('click', (e) => {
+		this.body.addEventListener('click', (e) => {
 			const replyBtn = e.target.closest('.replyBtn');
 			if (!replyBtn) return;
 
 			const replyContainer = e.target.closest('.main__replies-list');
 			const commentContainer = e.target.closest('.comments-container__section');
-			if (
-				this.removeTextarea() &&
-				replyBtn.classList.contains('replyBtn--active')
-			) {
+
+			e.target.classList.toggle('textarea--active');
+			if (!e.target.classList.contains('textarea--active')) {
+				this.removeExtraTextarea(e);
 				return;
 			}
-			console.log(replyContainer, commentContainer);
+
+			// this.removeTextarea();
+
 			commentContainer && this._setContainer(commentContainer) && handler();
 			replyContainer && this._setContainer(replyContainer) && handler();
-			this.removeActiveBtns();
-			replyBtn.classList.toggle('replyBtn--active');
 		});
 	}
 
-	_setContainer(container) {
-		this._parentElement = container;
-		return true;
+	removeExtraTextarea(e) {
+		const Alltextarea = e.target
+			.closest('.comments-container__list')
+			.querySelectorAll('.comments-container__write-comment');
+
+		Alltextarea.forEach((t) => t.remove());
 	}
 
-	textareaMarkUp(currentUser) {
-		const markup = `
-		<section class="comments-container__write-comment">
-			<form class="form">
-				<label for="write-comment">
-				<img class="writer-dp" src="${currentUser.image.png}" alt="writer-dp">
-				</label>
-				<textarea type="text" name="comment" class="textarea-fieled" id="write-comment" placeholder="Add a comment..." autofocus></textarea>
-				<button class="comment-btn" type="submit">reply</button>
-			</form>
-    	</section>
-		`;
-		console.log(this._parentElement);
-		this._parentElement.insertAdjacentHTML('afterEnd', markup);
+	// removeActiveBtns() {
+	// 	const btns = document.querySelectorAll('.replyBtn');
+	// 	btns.forEach((btn) => btn.classList.remove('replyBtn--active'));
+	// }
+
+	// _toggleActiveBtns(replyBtn) {
+	// 	if (replyBtn.classList.contains('replyBtn--active'))
+	// 		replyBtn.classList.remove('replyBtn--active');
+	// 	else replyBtn.classList.add('replyBtn--active');
+
+	// 	console.log('ues');
+	// }
+
+	// _isTextareaRemovedAndReplyBtnActive(replyBtn) {
+	// 	return (
+	// 		this.removeTextarea() && replyBtn.classList.contains('replyBtn--active')
+	// 	);
+	// }
+
+	_setContainer(container) {
+		this.parentElement = container;
+		return true;
 	}
 }
 
